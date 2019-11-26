51c76c5d3edb5e6367f9571be7fc34985cc9836d
AlanJschultz <alanjschultz@gmail.com>
2019-11-26 13:31:47 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+/*
+
+Project Overview
+
+We have consulted with our Industry Advisory Board and a major component
+for job consideration is that a student should be able to show a list of projects they have completed, along with easy to access information about your work history and current Github activity.Your portfolio is one link that can be provided to future employers to showcase your skills and present easily accessible information about you.
+
+Objectives:
+
+    Use Bootstrap to build a responsive application.
+Use modern front end development tools.
+Create one central domain
+for organizing projects, biographical information, and employer contact info.
+Portfolio Requirements
+
+For your portfolio, you’ re expected to have the following website elements included.
+
+Portfolio
+Landing / Header Page
+Navbar
+About Me
+Projects Page
+Tutorial Website
+Creature Assignment
+Api Project
+Contact Section
+In addition to the website elements above, you need to make sure you have done the following with your webpage:
+
+    The site is aesthetically pleasing overall and is tailored in a way that exhibits your unique professional personality.
+The CSS styling goes beyond the minimal components(e.g., background color and font color), and provides an engaging user experience.Use of CSS Variables
+for our file system is required.
+The site is highly navigable with its buttons.All buttons are functioning properly and even have features that accentuate the texture of the buttons.
+The site has thumbnails with links that show examples of work you’ ve built in this course.These will include samples or links, Codepen.io projects, your Tutorial Website Project, and any projects you 've done in the past or on the side. Essentially, these are links that showcase your original work.
+The site provides detailed information
+for employers to contact you, including LinkedIn and Github info / buttons.
+The site also has a working contact form using Formspree.io.
+The portfolio has an about feature that tells your story as a developer and professional.
+The site is free of spelling errors and typos.
+The site has been completed in a timely fashion.
+Your resume should be included and downloadable.
+The site is deployed to Github pages.
+Note: If you would like to omit something on this list
+for any reason, please talk to us in person about the omission and why.
+
+Resources:
+
+    Balsamiq(Links to an external site.)(free - great
+        for wireframing)
+Adobe XD(Links to an external site.)(free - another great tool to help with wireframing)
+Unsplash(Links to an external site.)(free - royalty free images)
+Pexels(Links to an external site.)(free - royalty free images)
+Palleton(Links to an external site.)(color scheme)
+Color - Picker(Links to an external site.)(chrome extension - allows you to pick color from any webpage)
+Colors(Links to an external site.)(color schemes generator)
+
+*/
